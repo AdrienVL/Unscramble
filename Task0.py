3cb371fb8c6a5ffdc4dcc0beb355502d140f1ecf
AdrienVL <adrienlhemann@gmail.com>
2021-03-27 11:44:37 -0500

    Task0 Complete

--- Task0.py
+++ Task0.py
@@ -20,3 +20,11 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+# Define first Record of texts
+firstTextRecord = texts[0]
+
+#Define last record of calls
+lastCallRecord = calls[len(calls)-1]
+
+print("First record of texts, {} texts {} at time {}.".format(firstTextRecord[0], firstTextRecord[1], firstTextRecord[2]))
+print("Last record of calls, {} texts {} at time {}.".format(lastCallRecord[0], lastCallRecord[1], lastCallRecord[2], lastCallRecord[3]))
